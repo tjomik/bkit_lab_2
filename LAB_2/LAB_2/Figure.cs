35e5c0ddac0cec200f56d75eda21d68387161b48
TJOMA\Artjoms <tjomik1996@gmail.com>
2017-10-25 09:05:47 +0300

    Commit
    Commit

---
+++ LAB_2/LAB_2/Figure.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+
+
+namespace LAB_2
+{
+    abstract class Figure
+    {
+       public string Type
+        {
+            get
+            {
+                return _Type;
+            }
+            set
+            {
+                _Type = value;
+            }
+        }
+        string _Type;
+        public abstract double FindArea();
+
+        public override string ToString()
+        {
+            return this.Type + " площадью " + this.FindArea().ToString();
+
+        }
+
+    }
+}
---
+++ LAB_2/LAB_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle a = new Rectangle(2, 5);
+            a.Print();
+            Square b = new Square(5);
+            b.Print();
+            Circle c = new Circle(2);
+            c.Print();
+            Console.ReadLine();
+        }
+    }
+}
